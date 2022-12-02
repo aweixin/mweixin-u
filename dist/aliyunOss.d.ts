fdb466842a316dc0919be2d7926163f679a1267c
DESKTOP-CP3BVDE\许致信 <13335309435@qq.com>
2022-12-02 17:11:23 +0800

    完善文档

--- dist/aliyunOss.d.ts
+++ dist/aliyunOss.d.ts
@@ -14,6 +14,21 @@ interface ossAuthInfoType {
  * @param {ossAuthInfoType} OssAuthInfo
  * 示例：
  * ```
+     async aliyunOss(filePath: any, path: string) {
+        Utils.delsys_expire('aliyunoss_' + path)
+        let aliyunoss_config = Utils.getsys('aliyunoss_' + path);
+        if (!aliyunoss_config) {
+            const { data } = await Server.post('http://192.168.199.218:10201/open/v1/file/getOssAuthInfo', {
+                path: path
+            });
+            aliyunoss_config = data;
+            Utils.setsys('aliyunoss_' + path, data, { key: 'seconds', value: aliyunoss_config.durationSeconds });
+        }
+
+        aliyunOss(filePath, aliyunoss_config);
+      }
+
+      //////////////////////////////////////////////////////////////////////////////////////
       aliyunOss(filePath,{
             accessKeyId: string
             accessKeySecret: string
